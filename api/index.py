ceeef73237484ef7f01e50ccd7ce67f7fd6f5547
Fotio-SynkCORE <preciousfotio@gmail.com>
2026-09-17 18:26:05 +0100

    Fix Vercel module resolution

--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
-import sys
-import os
-
-# Add parent directory (root) to Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from main import app  # type: ignore
+try:
+    from main import app
+except ImportError:
+    from ..main import app
