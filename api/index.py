03e687cfa87b3ba890ab45e94c5a87d1e58b910b
Fotio-SynkCORE <preciousfotio@gmail.com>
2026-09-17 17:51:57 +0100

    Fix Vercel import path in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
-from main import app
+import sys
+import os
+
+# Add root project folder to Python search path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from main import app # type: ignore
